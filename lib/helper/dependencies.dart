10cd52f3c0d3d647898fa31de7646bc156ed484d
Angelalpizar18 <angel_alpizar@live.com.mx>
2025-04-04 09:36:21 -0600

    Agregando Json DB

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,20 +1,19 @@
 import 'package:flutter_application_1/controllers/popular_product_controller.dart';
 import 'package:flutter_application_1/data/api/api_client.dart';
 import 'package:flutter_application_1/data/repository/popular_product_repo.dart';
+import 'package:flutter_application_1/utils/app_constants.dart';
 import 'package:get/get.dart';
 
 /// Función asincrónica de inicialización.
 /// `Future<void>` indica que la función no devuelve un valor concreto.
 /// Se usa `async` para permitir operaciones asincrónicas dentro de la función.
-Future <void> init()async {
+Future<void> init() async {
   //api client
-  Get.lazyPut(()=>ApiClient(appBaseUrl: "https://www.dbestech.com"));
+  Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.BASE_URL));
 
   //repository
-  Get.lazyPut(()=>PopularProductRepo(apiClient: Get.find()));
+  Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
 
   //Controller
-  Get.lazyPut(()=>PopularProductController(popularProductRepo: Get.find()));
-
-  
+  Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
 }
